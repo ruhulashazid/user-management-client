33033aa8ec2da0f522e39ad40bab4052734a2efb
ruhulashazid <ruhulaminshazid@gmail.com>
2024-09-19 23:38:11 +0600

    Welcome message add

--- src/App.js
+++ src/App.js
@@ -9,8 +9,23 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 function App() {
     return (
         <Router>
-            <div className="App">
+            <div className="App" style={{ 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                height: '100vh', 
+                textAlign: 'center' 
+            }}>
                 <Routes>
+                    {/* Welcome message at the root route */}
+                    <Route path="/" element={
+                        <div>
+                            <h1>Welcome to User Management</h1>
+                            <p>Please register, log in, or manage users.</p>
+                        </div>
+                    } />
+
+                    {/* Other routes */}
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/users" element={<UserManagement />} />
